4d90cc43e5b3fd430642efadbfd87849cd89dbc8
Finn <finnmiddletonbaird@gmail.com>
2022-04-08 19:27:38 +0100

    Added multi-agent gym simulation in env_wrapper. Removed unused algorithms from __init__

--- py_agent/agent.py
+++ py_agent/agent.py
@@ -6,282 +6,222 @@
 # Imports
 #-----------------------------------------------------------------------------------------------------------
 
-import os, sys, subprocess
-import argparse
+import os
 import ssl
 import asyncio
 import logging
-import gym
-import gym_maze
 
 from contextlib import AsyncExitStack, asynccontextmanager
 from asyncio_mqtt import Client, Will, MqttError
 from dotenv import load_dotenv
-from random import randint
 
 #-----------------------------------------------------------------------------------------------------------
-# Functions
+# Classes
 #-----------------------------------------------------------------------------------------------------------
 
-def get_args():
+class sim_agent():
     """
-        function to get the command line arguments
-
-        returns a namespace of arguments
+        class for simulated agent, contains client and all methods required by MQTT and agent
     """
-    parser = argparse.ArgumentParser()
+    def __init__(self):
+        """
+            function to init simulated agent class
+        """
+        #MQTT credentials stored in .env file
+        load_dotenv()
+        MQTT_HOST = os.getenv("MQTT_HOST")
+        MQTT_USERNAME = os.getenv("MQTT_USERNAME")
+        MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
+    
+        #init client, message queue and start flag
+        self.client = Client(MQTT_HOST, port=8883, username=MQTT_USERNAME, password=MQTT_PASSWORD, tls_context=ssl.create_default_context())
+        self.msg_q = asyncio.Queue()
+        self.start_flag = asyncio.Event()
 
-    parser.add_argument("--verbose", "-v", action="count", default=0, help="Increase verbosity level")
+    async def post_to_topic(self, topic, msg, retain=False):
+        """
+            coroutine to publish messages to topics to an mqtt broker connected to by client
+    
+            client is the mqtt client object
 
-    return parser.parse_args()
+            topic is an string of topic to be published to
 
-async def post_to_topic(client, topic, msg, retain=False):
-    """
-        coroutine to publish messages to topics to an mqtt broker connected to by client
+            msgs is the message to be published to the topic
+    
+            retain is a bool to determine if the message should be retained in the topic by the broker defaults to False
+        """
+        logging.debug("Publishing %s to %s", msg, topic)
+        await self.client.publish(topic, msg, qos=1, retain=retain)
+        await asyncio.sleep(2)
 
-        client is the mqtt client object
+    async def process_messages(self, msgs):
+        """
+            coroutine to process incoming messages and put them in a queue
 
-        topic is an string of topic to be published to
-
-        msgs is the message to be published to the topic
-
-        retain is a bool to determine if the message should be retained in the topic by the broker defaults to False
-    """
-    logging.debug("Publishing %s to %s", msg, topic)
-    await client.publish(topic, msg, qos=1, retain=retain)
-    await asyncio.sleep(2)
-
-async def process_messages(msgs, queue):
-    """
-        coroutine to process incoming messages and put them in a queue
-
-        msgs is an async constructor of messages
-
-        queue is the queue to put the messages in
-    """
-    async for msg in msgs:
-        topic = msg.topic
-        payload = msg.payload.decode()
-        logging.debug("%s received from topic %s", payload, topic)
-        q_item = f'{topic}:{payload}'
-        await queue.put(q_item)
-
-async def process_status(msgs, status_flag):
-    """
-        coroutine to process incoming status messages and set appropriate flag
-
-        msgs is an async constructor of messages
-
-        status_flag is an Event to be set or cleared
-    """
-    async for msg in msgs:
-        status = msg.payload.decode()
-        await asyncio.sleep(3)
-
-        if status:
-            status_flag.set()
-        elif not status:
-            status_flag.clear()
-
-async def get_item(desired_topic, queue):
-    """
-        coroutine to get an item from a queue with a specific topic
-
-        desired topic is a string of the desired topic
-
-        queue is the queue from which to get the item
-    """
-    topic = None
-
-    #loop until desired topic is found
-    while topic != desired_topic:
-        q_item = await queue.get()
-        topic = q_item.split(':')[0]
-        payload = q_item.split(':')[1]
-
-        #if desired topic then return payload
-        if topic == desired_topic:
-            return payload
-        #else return item to queue for later processing
-        else:
+            msgs is an async constructor of messages
+        """
+        async for msg in msgs:
+            topic = msg.topic
+            payload = msg.payload.decode()
+            logging.debug("%s received from topic %s", payload, topic)
             q_item = f'{topic}:{payload}'
-            await queue.put(q_item)
+            await self.msg_q.put(q_item)
 
-async def main_loop(stack, tasks, client, msg_q, status_flag):
-    """
-        coroutine to simulate main function of agent over MQTT:
-            1. get an index
-            2. publish init obv
-            3. get actions
-            4. publish obv, reward and done
-
-        stack is the asyncronous stack that runs the app
-
-        tasks is a set of asyncronous tasks being run
-
-        client is the mqtt client object
-
-        msg_q is a queue for this agents received messages
-    """
-    #init env
-    env = gym.make("maze-sample-5x5-v0", enable_render=False)
-
-    start_flag = asyncio.Event()
-
-    await post_to_topic(client, ("/agents/add"), (1))
-
-    #get index from queue
-    n = int(await get_item("/agents/index", msg_q))
-    logging.info("Agent index: %u", n)
-
-    #unsubscribe from index topic when this agent has an index
-    await client.unsubscribe("/agents/index")
-
-    #post to agent n status
-    await post_to_topic(client, (f'/agents/{n}/status'), (1), retain=True)
+    async def process_status(self, msgs):
+        """
+            coroutine to process incoming status messages and set appropriate flag
     
-    manager = client.filtered_messages((f'/agents/{n}/action'))
-    msgs = await stack.enter_async_context(manager)
-    task = asyncio.create_task(process_messages(msgs, msg_q))
-    tasks.add(task)
+            msgs is an async constructor of messages
+        """
+        async for msg in msgs:
+            status = msg.payload.decode()
+            await asyncio.sleep(3)
 
-    manager = client.filtered_messages((f'/agents/{n}/start'))
-    msgs = await stack.enter_async_context(manager)
-    task = asyncio.create_task(process_status(msgs, start_flag))
-    tasks.add(task)
+            if status:
+                self.start_flag.set()
+            elif not status:
+                self.start_flag.clear()
 
-    await client.subscribe(f'/agents/{n}/action')
-    await client.subscribe(f'/agents/{n}/start')
-
-    #wait for master to initialise agent
-    logging.info("Waiting for start...")
-    await start_flag.wait()
-    logging.info("Starting simulation")
-
-    for e in range(100):
-        #get initial observation
-        obv = env.reset()
-        done = False
-
-        #post initial observation
-        await post_to_topic(client, (f'/agents/{n}/obv'), (f'{obv}'))
-
-        total_reward = 0
-
-        for t in range(10000):
-            #get action
-            action = int(await get_item(f'/agents/{n}/action', msg_q)) 
-
-            logging.debug("Perform action: %u", action)
-
-            obv, reward, done, info = env.step(action)
-
-            #post to relevant topics
-            agent_topics = (f'/agents/{n}/obv', f'/agents/{n}/reward', f'/agents/{n}/done')
-            agent_msgs = [f'{obv}', f'{reward}', f'{done}']
-
-            logging.debug("Observation: %s", obv)
-            logging.debug("Reward: %i", reward)
-            logging.debug("Done: %s", done)
-
-            for topic, msg in zip(agent_topics, agent_msgs):
-                await post_to_topic(client, topic, msg)
-
-            total_reward += reward
-
-            if done:
-                logging.info(f'Episode {e} completed with reward: {total_reward}')
-                break
-
-            if t >= 9999:
-                logging.info(f'Episode {e} timed out with reward: {total_reward}')
-                break;
-
-async def cancel_tasks(tasks):
-    """
-        coroutine to cancel all tasks and clean up on exit
-    """
-    for task in tasks:
-        if task.done():
-            continue
+    async def get_item(self, desired_topic):
+        """
+            coroutine to get an item from a queue with a specific topic
         
-        try:
-            task.cancel()
-            await task
-        except asyncio.CancelledError:
-            pass
+            desired topic is a string of the desired topic
+        """
+        topic = None
 
-async def main():
-    """
-        main coroutine
-    """
-    #MQTT credentials stored in .env file
-    load_dotenv()
-    MQTT_HOST = os.getenv("MQTT_HOST")
-    MQTT_USERNAME = os.getenv("MQTT_USERNAME")
-    MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
-
-    async with AsyncExitStack() as stack:
-        msg_q = asyncio.Queue()
-        status_flag = asyncio.Event()
-        tasks = set()
-        stack.push_async_callback(cancel_tasks, tasks)
-
-        client = Client(MQTT_HOST, port=8883, username=MQTT_USERNAME, password=MQTT_PASSWORD, tls_context=ssl.create_default_context())
-        await stack.enter_async_context(client)
-        logging.info("MQTT Connected")
-
-        #set up message handler for index topic
-        manager = client.filtered_messages(("/agents/index"))
-        msgs = await stack.enter_async_context(manager)
-        task = asyncio.create_task(process_messages(msgs, msg_q))
-        tasks.add(task)
-
-        await client.subscribe("/agents/index")
-
-        #start main task
-        task = asyncio.create_task(main_loop(stack, tasks, client, msg_q, status_flag))
-        tasks.add(task)
-
-        await asyncio.gather(*tasks)
-
-#-----------------------------------------------------------------------------------------------------------
-# main
-#-----------------------------------------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    #init logging
-    logging.basicConfig(format="%(asctime)s.%(msecs)03d: %(levelname)s: %(message)s", datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
-
-    if not hasattr(logging, "VDEBUG") and not hasattr(logging, "vdebug") and not hasattr(logging.getLoggerClass(), "vdebug"):
-        #add new logging level "vdebug" to logger if not already added
-        def logForLevel(self, message, *args, **kwargs):
-            if self.isEnabledFor(logging.DEBUG - 5):
-                self._log(logging.DEBUG - 5, message, args, **kwargs)
-
-        def logToRoot(message, *args, **kwargs):
-            logging.log(logging.DEBUG - 5, message, *args, **kwargs)
-
-        logging.addLevelName(logging.DEBUG - 5, "VDEBUG")
+        #loop until desired topic is found
+        while topic != desired_topic:
+            q_item = await self.msg_q.get()
+            topic = q_item.split(':')[0]
+            payload = q_item.split(':')[1]
     
-        setattr(logging, "VDEBUG", logging.DEBUG - 5)
-        setattr(logging.getLoggerClass(), "vdebug", logForLevel)
-        setattr(logging, "vdebug", logToRoot)
+            #if desired topic then return payload
+            if topic == desired_topic:
+                return payload
+            #else return item to queue for later processing
+            else:
+                q_item = f'{topic}:{payload}'
+                await self.msg_q.put(q_item)
+
+    async def run(self, stack, tasks, index_flag, env_q, obv_flag, action_flag, reward_flag, done_flag):
+        """
+            coroutine to simulate main function of agent over MQTT:
+                1. get an index
+                2. publish init obv
+                3. get actions
+                4. publish obv, reward and done
+    
+            stack is the asyncronous stack that runs the app
+
+            tasks is a set of asyncronous tasks being run
+
+            index_flag is a flag that is set when this agent can get an index from the master
+            this flag is set by the previous agent such that two agents do not have the same index
+
+            env_q is the queue to send and receive data from the env
+
+            obv_flag is a flag that is set if obv is in env_q
+
+            action_flag is a flag that is set if action is in env_q
+
+            reward_flag is a flag that is set if reward is in env_q
+
+            done_flag is a flag that is set if done is in env_q
+        """
+        await stack.enter_async_context(self.client)
+        logging.info("MQTT Connected")
+    
+        #set up message handler for index topic
+        manager = self.client.filtered_messages(("/agents/index"))
+        msgs = await stack.enter_async_context(manager)
+        task = asyncio.create_task(self.process_messages(msgs))
+        tasks.add(task)
+    
+        await self.client.subscribe("/agents/index")
+
+        #wait to ensure this agent will get individual index
+        await index_flag.wait()
+
+        #clear flag to ensure only this agent gets the next index
+        index_flag.clear()
+    
+        await self.post_to_topic(("/agents/add"), (1))
+
+        #get index from queue
+        n = int(await self.get_item("/agents/index"))
+        logging.info("Agent index: %u", n)
+    
+        #unsubscribe from index topic when this agent has an index
+        await self.client.unsubscribe("/agents/index")
+    
+        #post to agent n status
+        await self.post_to_topic((f'/agents/{n}/status'), (1), retain=True)
+        
+        #set index flag as this agent has an index
+        index_flag.set()
+        
+        manager = self.client.filtered_messages((f'/agents/{n}/action'))
+        msgs = await stack.enter_async_context(manager)
+        task = asyncio.create_task(self.process_messages(msgs))
+        tasks.add(task)
+
+        manager = self.client.filtered_messages((f'/agents/{n}/start'))
+        msgs = await stack.enter_async_context(manager)
+        task = asyncio.create_task(self.process_status(msgs))
+        tasks.add(task)
+
+        await self.client.subscribe(f'/agents/{n}/action')
+        await self.client.subscribe(f'/agents/{n}/start')
+    
+        #wait for master to initialise agent
+        logging.info("Agent %u waiting for start...", n)
+        await self.start_flag.wait()
+        logging.info("Starting agent %u simulation", n)
+
+        for e in range(100):
+            #wait for obv_flag to be set to show obv in env queue
+            await obv_flag.wait()
+            #get initial observation
+            obv = await env_q.get()
+            #clear obv_flag to show obv removed from env queue
+            obv_flag.clear()
+
+            done = False
+        
+            #post initial observation
+            await self.post_to_topic((f'/agents/{n}/obv'), (f'{obv}'))
+    
+            for t in range(10000):
+                #get action from mqtt and put into env queue
+                action = int(await self.get_item(f'/agents/{n}/action')) 
+                await env_q.put(action)
+                action_flag.set()
+    
+                #get obv, reward and done from env queue
+                await obv_flag.wait()
+                obv = await env_q.get()
+                obv_flag.clear()
+
+                await reward_flag.wait()
+                reward = await env_q.get()
+                reward_flag.clear()
+
+                await done_flag.wait()
+                done = await env_q.get()
+                done_flag.clear()
+    
+                #post to relevant topics
+                agent_topics = (f'/agents/{n}/obv', f'/agents/{n}/reward', f'/agents/{n}/done')
+                agent_msgs = [f'{obv}', f'{reward}', f'{done}']
+
+                for topic, msg in zip(agent_topics, agent_msgs):
+                    await self.post_to_topic(topic, msg)
+    
+                if done:
+                    break
+
+                if t >= 9999:
+                    break;
 
 
-    #global arguments so that can be accessed by any coroutine
-    global args
-    args = get_args()
-
-    #set more verbose logging level, default is info (verbose == 0)
-    if args.verbose == 1:
-        logging.getLogger().setLevel(logging.DEBUG)
-    elif args.verbose == 2:
-        logging.getLogger().setLevel(logging.VDEBUG)
-    elif args.verbose > 2:
-        logging.warning("Maximum verbosity level is 2; logging level set to verbose debug (verbosity level 2).")
-        logging.getLogger().setLevel(logging.VDEBUG)
-
-    asyncio.run(main())
-
-    sys.exit(0)
